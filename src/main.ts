82a3d01052c10dd943093272cfa2e6d8f630207b
MARCIO BINCOLETO JUNIOR <marcio.bjunior@senacsp.edu.br>
2025-03-25 21:54:32 -0300

    feito instalação swaggerModule, add informações de AptiProperty e Optinal

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,18 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   )
+
+  const config = new DocumentBuilder()
+  .setTitle('Api Filmes e Series')
+  .setDescription('A presente API tem como objetivo simular cadastro', )
+  .setVersion('1.0')
+  .addTag('usuario')
+  .addTag('filme')
+  .addTag('serie')
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   
   useContainer(app.select(AppModule), {fallbackOnErrors: true})
   app.enableCors();
